5dff1ae0ec7f168a36b5e2fcf2a242c48ebdfeec
jjasonlan <jjasonlan@users.noreply.github.com>
2018-02-23 14:36:45 -0800

    Add files via upload

--- Intro to Solidity/contracts/fibonacci.sol
+++ Intro to Solidity/contracts/fibonacci.sol
@@ -3,10 +3,23 @@ pragma solidity 0.4.19;
 
 contract Fibonacci {
     /* Carry out calculations to find the nth Fibonacci number */
-    function fibRecur(uint n) public pure returns (uint) {
-    }
+    // function fibRecur(uint n) public pure returns (uint) {
+    // }
 
     /* Carry out calculations to find the nth Fibonacci number */
-    function fibIter(uint n) public returns (uint) {
+	function fibIter(uint n) public returns (uint) {
+        if (n == 1) {
+            return 1;
+        }
+        uint counter = 2;
+        uint first = 1;
+        uint second = 1;
+        while (counter < n) {
+            uint temp = first + second;
+            first = second;
+            second = temp;
+            counter = counter + 1;
+        }
+        return second;
     }
 }
